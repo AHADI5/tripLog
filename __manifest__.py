f8da518ba186dcc144cbc17effeac2f31e919214
Grace_TASSI <gracektassi@gmail.com>
2024-11-08 14:23:10 +0200

    module creation: correction

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*- 
+{
+    "name":"Trip log",
+    "summary":"Virunga Alliance Trip log",
+    "version":"17.0.1.0.0",
+    'description': 'Virunga Alliance Trip log',
+    'author': 'Virunga Foundation',
+    "license":"OPL-1",
+    "depends":["base"],
+    "application":True
+}
